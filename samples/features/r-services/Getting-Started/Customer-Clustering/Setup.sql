d1e2ec39f2503e7c5206c6d9112f16336357e7b6
Nellie Gustafsson <negust@microsoft.com>
2017-01-27 15:28:07 -0800

    updated getting started samples

--- samples/features/r-services/Getting-Started/Customer-Clustering/Setup.sql
+++ samples/features/r-services/Getting-Started/Customer-Clustering/Setup.sql
@@ -5,9 +5,9 @@
 -- to match the directories of your installation!
 USE master;
 GO
-RESTORE DATABASE Tpcx1b
-   FROM DISK = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Backup\Tpcx1b.bak'
+RESTORE DATABASE tpcxbb_1gb
+   FROM DISK = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Backup\tpcxbb_1gb.bak'
    WITH
-                MOVE 'Tpcx1b' TO 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\Tpcx1b.mdf'
-                ,MOVE 'Tpcx1b_log' TO 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\Tpcx1b.ldf';
+                MOVE 'tpcxbb_1gb' TO 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\tpcxbb_1gb.mdf'
+                ,MOVE 'tpcxbb_1gb_log' TO 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\tpcxbb_1gb.ldf';
 GO
