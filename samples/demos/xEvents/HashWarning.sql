5e2ae92fd021400a6ccb0ff410f4705c3ee03e67
Amit Banerjee <amit.banerjee@microsoft.com>
2017-02-04 02:29:23 -0800

    Rename samples/demos/mssqltiger/xEvents/HashWarning.sql to samples/demos/xEvents/HashWarning.sql

--- samples/demos/xEvents/HashWarning.sql
+++ samples/demos/xEvents/HashWarning.sql
@@ -42,7 +42,7 @@ ADD TARGET package0.event_file(SET filename=N'C:\IP\Tiger\TR23\Demos\Demo 1.1 - 
 WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=30 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=OFF)
 GO
  
---Execute the stored procedure first with parameter value ‘WA’ – which will select 1% of data. 
+--Execute the stored procedure first with parameter value â€˜WAâ€™ â€“ which will select 1% of data. 
 DBCC FREEPROCCACHE
 GO
 ALTER EVENT SESSION [HashSpills] ON SERVER STATE = START
