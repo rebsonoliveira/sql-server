555b9eda2922225259480b3f545b52d56846b5c0
Jovan Popovic <jocapc@gmail.com>
2017-03-11 23:46:35 +0100

    Refactored node todo sample

--- samples/features/json/todo-app/nodejs-express4-rest-api/db.js
+++ samples/features/json/todo-app/nodejs-express4-rest-api/db.js
@@ -30,40 +30,47 @@ function createRequest(query, connection) {
 
 function stream (query, connection, output, defaultContent) {
     
-    errorHandler = function (ex) { throw ex; };
     var request = query;
     if (typeof query == "string") {
         request = this.createRequest(query, connection);
     }
-    
+    pipe(request, output, defaultContent);
+}
+
+function pipe (request, output, defaultContent) {
     var empty = true;
     request.on('row', function (columns) {
         empty = false;
         output.write(columns[0].value);
     });
-    
+
     request.on('done', function (rowCount, more, rows) {
-        if (empty) {
-            output.write(defaultContent);
-        }
-        output.end();
+        _OnDone(empty, defaultContent, output);
     });
-    
+
     request.on('doneProc', function (rowCount, more, rows) {
-        if (empty) {
+        _OnDone(empty, defaultContent, output);
+    }); 
+}
+
+function _OnDone(empty, defaultContent, output) { 
+        if(empty) { 
             output.write(defaultContent);
-        }
-        output.end();
-    });
-    
-    connection.on('connect', function (err) {
-        if (err) {
-            throw err;
-        }
-        connection.execSql(request);
-    });
+        } 
+        try { output.end(); } catch (err) { /* Ignore stream close error. */ }
+    }
+
+function executeRequest (request, connection) { 
+        connection.on('connect', function (err) {
+            if (err) {
+                throw err;
+            }
+            connection.execSql(request);
+        });
 }
 
 module.exports.createConnection = createConnection;
-module.exports.createRequest = createRequest;
+module.exports.createRequest = createRequest; 
+module.exports.executeRequest = executeRequest;
 module.exports.stream = stream;
+module.exports.pipe = pipe;
