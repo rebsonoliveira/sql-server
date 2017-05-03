71043482945baef16f9040c95eae28d4ef4394ab
Jovan Popovic <jocapc@gmail.com>
2017-05-03 18:00:03 +0200

    Fixed bug in todo app

--- samples/features/json/todo-app/nodejs-express4-rest-api/app.js
+++ samples/features/json/todo-app/nodejs-express4-rest-api/app.js
@@ -2,7 +2,7 @@
 var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.raw());
+app.use(bodyParser.text()); 
 app.use('/todo', require('./routes/todo'));
 
 // catch 404 and forward to error handler
