6567045fb56438b0fc21bee540785cf54879eb7f
Yassine <ky.resourcebox@gmail.com>
2017-05-30 09:50:10 +0200

    Update setup.sql

--- samples/features/machine-learning-services/python/getting-started/bike-sharing prediction/setup.sql
+++ samples/features/machine-learning-services/python/getting-started/bike-sharing prediction/setup.sql
@@ -1,5 +1,5 @@
 ﻿-- Before we start, we need to restore the database
--- Step1: Download the compressed backup file (url)
+-- Step1: Download the backup file (url)
 --Save the file on a location where SQL Server can access it. For example: C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\Backup\
 
 -- In a new query window in SSMS, execute the following restore statement, but REMEMBER TO CHANGE THE FILE PATHS
