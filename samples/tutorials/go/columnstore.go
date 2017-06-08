0da307f15d89c78b619ec3fa4f006f6274bf8825
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-06-09 08:59:02 +0900

    gofmt

--- samples/tutorials/go/columnstore.go
+++ samples/tutorials/go/columnstore.go
@@ -1,10 +1,13 @@
 package main
 
-import _ "github.com/denisenkom/go-mssqldb"
-import "database/sql"
-import "log"
-import "fmt"
-import "time"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
 
 var server = "localhost"
 var port = 1433
@@ -14,33 +17,33 @@ var database = "SampleDB"
 
 // Delete an employee from database
 func ExecuteAggregateStatement(db *sql.DB) {
-    result, err := db.Prepare("SELECT SUM(Price) as sum FROM Table_with_5M_rows")
-    if err != nil {
-        fmt.Println("Error preparing query: " + err.Error())
-    }
-    
-    row := result.QueryRow()
-    var sum string
-    err = row.Scan(&sum)
-    fmt.Printf("Sum: %s\n", sum)
+	result, err := db.Prepare("SELECT SUM(Price) as sum FROM Table_with_5M_rows")
+	if err != nil {
+		fmt.Println("Error preparing query: " + err.Error())
+	}
+
+	row := result.QueryRow()
+	var sum string
+	err = row.Scan(&sum)
+	fmt.Printf("Sum: %s\n", sum)
 }
 
 func main() {
-    // Connect to database
-    connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;", 
-                                server, user, password, port, database)
-    conn, err := sql.Open("mssql", connString)
-    if err != nil {
-        log.Fatal("Open connection failed:", err.Error())
-    }
-    fmt.Printf("Connected!\n")
-    defer conn.Close()
+	// Connect to database
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+		server, user, password, port, database)
+	conn, err := sql.Open("mssql", connString)
+	if err != nil {
+		log.Fatal("Open connection failed:", err.Error())
+	}
+	fmt.Printf("Connected!\n")
+	defer conn.Close()
 
 	t1 := time.Now()
 	fmt.Printf("Start time: %s\n", t1)
-    
-    ExecuteAggregateStatement(conn)
+
+	ExecuteAggregateStatement(conn)
 
 	t2 := time.Since(t1)
-    fmt.Printf("The query took: %s\n", t2)
+	fmt.Printf("The query took: %s\n", t2)
 }
--- samples/tutorials/go/orm.go
+++ samples/tutorials/go/orm.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/mssql"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
 )
 
 var server = "localhost"
@@ -15,49 +16,49 @@ var database = "SampleDB"
 type User struct {
 	gorm.Model
 	FirstName string
-	LastName string
+	LastName  string
 }
 
 type Task struct {
 	gorm.Model
-	Title string
-	DueDate string
+	Title      string
+	DueDate    string
 	IsComplete bool
-	UserID  uint
+	UserID     uint
 }
 
-func ReadAllTasks(db *gorm.DB){
+func ReadAllTasks(db *gorm.DB) {
 	var users []User
 	var tasks []Task
 	db.Find(&users)
-	
-	for _, user := range users{
+
+	for _, user := range users {
 		db.Model(&user).Related(&tasks)
 		fmt.Printf("%s %s's tasks:\n", user.FirstName, user.LastName)
 		for _, task := range tasks {
-			fmt.Printf("Title: %s\nDueDate: %s\nIsComplete:%t\n\n", 
-							task.Title, task.DueDate, task.IsComplete)
+			fmt.Printf("Title: %s\nDueDate: %s\nIsComplete:%t\n\n",
+				task.Title, task.DueDate, task.IsComplete)
 		}
 	}
 }
 
-func UpdateSomeonesTask(db *gorm.DB, userId int){
+func UpdateSomeonesTask(db *gorm.DB, userId int) {
 	var task Task
 	db.Where("user_id = ?", userId).First(&task).Update("Title", "Buy donuts for Luis")
-	fmt.Printf("Title: %s\nDueDate: %s\nIsComplete:%t\n\n", 
-					task.Title, task.DueDate, task.IsComplete)
+	fmt.Printf("Title: %s\nDueDate: %s\nIsComplete:%t\n\n",
+		task.Title, task.DueDate, task.IsComplete)
 }
 
-func DeleteSomeonesTasks(db *gorm.DB, userId int){
+func DeleteSomeonesTasks(db *gorm.DB, userId int) {
 	db.Where("user_id = ?", userId).Delete(&Task{})
 	fmt.Printf("Deleted all tasks for user %d", userId)
 }
 
 func main() {
-	connectionString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s", 
-										server, user, password, port, database)
+	connectionString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s",
+		server, user, password, port, database)
 	db, err := gorm.Open("mssql", connectionString)
-	
+
 	if err != nil {
 		panic("failed to connect database")
 	}
@@ -70,14 +71,14 @@ func main() {
 
 	// Create awesome Users
 	fmt.Println("Creating awesome users...")
-	db.Create(&User{FirstName: "Andrea", LastName: "Lam"}) 		//UserID: 1
-	db.Create(&User{FirstName: "Meet", LastName: "Bhagdev"}) 	//UserID: 2
-	db.Create(&User{FirstName: "Luis", LastName: "Bosquez"})	//UserID: 3
+	db.Create(&User{FirstName: "Andrea", LastName: "Lam"})   //UserID: 1
+	db.Create(&User{FirstName: "Meet", LastName: "Bhagdev"}) //UserID: 2
+	db.Create(&User{FirstName: "Luis", LastName: "Bosquez"}) //UserID: 3
 
 	// Create appropriate Tasks for each user
 	fmt.Println("Creating new appropriate tasks...")
 	db.Create(&Task{
-		Title: "Do laundry", DueDate: "2017-03-30", IsComplete: false, UserID: 1}) 
+		Title: "Do laundry", DueDate: "2017-03-30", IsComplete: false, UserID: 1})
 	db.Create(&Task{
 		Title: "Mow the lawn", DueDate: "2017-03-30", IsComplete: false, UserID: 2})
 	db.Create(&Task{
@@ -92,7 +93,7 @@ func main() {
 	// Update - update Task title to something more appropriate
 	fmt.Println("Updating Andrea's task...")
 	UpdateSomeonesTask(db, 1)
-	
+
 	// Delete - delete Luis's task
 	DeleteSomeonesTasks(db, 3)
 }
