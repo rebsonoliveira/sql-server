21a2b47c2024949733604fc7d871408189fb4ad2
Jos de Bruijn <jodebrui@microsoft.com>
2017-06-26 10:08:07 -0700

    minor updates

--- samples/databases/wide-world-importers/wwi-dw-ssdt/wwi-dw-ssdt/SampleVersion.sql
+++ samples/databases/wide-world-importers/wwi-dw-ssdt/wwi-dw-ssdt/SampleVersion.sql
@@ -4,8 +4,9 @@
 	MinorSampleVersion INT NOT NULL,
 	MinSQLServerBuild NVARCHAR(25) NOT NULL,
 	[RowCount] INT NOT NULL DEFAULT (1),
-	CONSTRAINT uq_SampleVersion_RowCount UNIQUE ([RowCount]),
-	CONSTRAINT chk_SampleVersion_cardinality 
+	CONSTRAINT uq_SampleVersion_RowCount 
+	  UNIQUE ([RowCount]),
+	CONSTRAINT chk_SampleVersion_Cardinality 
 	  CHECK ([RowCount]= 1)
 )
 
