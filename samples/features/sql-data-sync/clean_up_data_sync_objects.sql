7f9b8594b74e62ad079dca3cafcffe4dd9a6e6af
Douglas Laudenschlager <douglasl@microsoft.com>
2017-10-04 12:06:48 -0700

    Fixed SQL syntax and cleaned up comments.

--- samples/features/sql-data-sync/clean_up_data_sync_objects.sql
+++ samples/features/sql-data-sync/clean_up_data_sync_objects.sql
@@ -1,37 +1,34 @@
---We want to drop all user table start with datasync and contains _dss
+-- List Data Sync user tables
 select * from sys.tables as st join sys.schemas as ss on ss.schema_id = st.schema_id 
-where ss.name = 'DataSync' and st.name like '%_dss%' and st.name like '%<tablename>%
+where ss.name = 'DataSync' and st.name like '%_dss%' and st.name like '%<tablename>%'
 
---generate the script to drop data sync tables
+-- Generate the script to drop Data Sync tables
 select 'Drop table [DataSync].['+ st.name+ '];' from sys.tables as st join sys.schemas as ss on ss.schema_id = st.schema_id 
-where ss.name = 'DataSync' and st.name like '%_dss%' and st.name like '%<tablename>%
+where ss.name = 'DataSync' and st.name like '%_dss%' and st.name like '%<tablename>%'
 
--- list data sync store procedures
+-- List Data Sync stored procedures
 select * from sys.procedures as sp join sys.schemas as ss on ss.schema_id = sp.schema_id 
-where ss.name = 'DataSync' and sp.name like '%_dss_%'  and sp.name like '%<tablename>%
+where ss.name = 'DataSync' and sp.name like '%_dss_%'  and sp.name like '%<tablename>%'
 
---- generate the script to drop data sync store procedures
+--- Generate the script to drop Data Sync stored procedures
 select 'Drop procedure [DataSync].['+ sp.name+ '];' from sys.procedures as sp join sys.schemas as ss on ss.schema_id = sp.schema_id 
-where ss.name = 'DataSync' and sp.name like '%_dss_%' and sp.name like '%<tablename>%
+where ss.name = 'DataSync' and sp.name like '%_dss_%' and sp.name like '%<tablename>%'
 
--- list triggers for data sync
+-- List Data Sync triggers
 select * from sys.triggers as st
-where st.name like '%_dss%' and st.name like '%trigger' and st.name like '%<tablename>%
+where st.name like '%_dss%' and st.name like '%trigger' and st.name like '%<tablename>%'
 
---delete datasync triggers if there is any
+-- Generate the script to drop Data Sync triggers
 select 'Drop trigger ['+st.name+']' from sys.triggers as st
-where st.name like '%_dss%' and st.name like '%trigger' and st.name like '%<tablename>%
+where st.name like '%_dss%' and st.name like '%trigger' and st.name like '%<tablename>%'
 
-
--- list user-define for data sync
-
+-- List Data Sync UDTs
 select * from sys.types as st join 
 sys.schemas as ss on st.schema_id = ss.schema_id 
-where ss.name = 'DataSync' and st.name like '%_dss_%' and st.name like '%<tablename>%
+where ss.name = 'DataSync' and st.name like '%_dss_%' and st.name like '%<tablename>%'
 
---- generate script for dropping data sync-related  udtt
+-- Generate the script to drop Data Sync UDTs
 select 'Drop Type  [DataSync].['+ st.name+ '];' 
 from sys.types as st join 
 sys.schemas as ss on st.schema_id = ss.schema_id 
-where ss.name = 'DataSync' and st.name like '%_dss_%' and st.name like '%<tablename>%
-
+where ss.name = 'DataSync' and st.name like '%_dss_%' and st.name like '%<tablename>%'
