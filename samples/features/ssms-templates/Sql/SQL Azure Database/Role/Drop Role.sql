80d5bf59f8df98a4b1bc910a9424c77647896f00
shueybubbles <david.shiflet@microsoft.com>
2018-05-06 12:19:40 -0400

    sql samples

---
+++ samples/features/ssms-templates/Sql/SQL Azure Database/Role/Drop Role.sql
@@ -0,0 +1,7 @@
+-- ========================================================================================
+-- Drop Database Role template for Azure SQL Database and Azure SQL Data Warehouse Database
+-- ========================================================================================
+
+-- Drop the role
+DROP ROLE <role_name, sysname, Production_Owner>;
+GO
