cf21a42db7bc8b1b5696197bd7ee1fa3a4503d4b
Jovan Popovic <jocapc@gmail.com>
2018-09-10 16:30:22 +0200

    Addressed UC's comments

--- samples/databases/wide-world-importers/wwi-ssdt/wwi-ssdt/Application/Tables/Logs.sql
+++ samples/databases/wide-world-importers/wwi-ssdt/wwi-ssdt/Application/Tables/Logs.sql
@@ -1,16 +1,16 @@
 ﻿CREATE TABLE [Application].[Logs](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Message] [nvarchar](max) NULL,
-	[Level] [nvarchar](128) NULL,
-	[TimeStamp] [datetime] NOT NULL,
-	[LogEvent] [nvarchar](max) NULL,
-	INDEX cci CLUSTERED COLUMNSTORE
+	[Message] NVARCHAR(max) NULL,
+	[Level] NVARCHAR(128) NULL,
+	[EventTime] DATETIME2 (7) NOT NULL,
+	[LogEvent] NVARCHAR(max) NULL,
+	INDEX CCX_Application_Logs CLUSTERED COLUMNSTORE
 )
 GO
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'Description', @value = 'CLUSTERED COLUMNSTORE INDEX that compress application log.', @level0type = N'SCHEMA', @level0name = N'Application', @level1type = N'TABLE', @level1name = N'Logs', @level2type = N'INDEX', @level2name = N'cci';
+EXECUTE sp_addextendedproperty @name = N'Description', @value = 'CLUSTERED COLUMNSTORE INDEX that compress application log.', @level0type = N'SCHEMA', @level0name = N'Application', @level1type = N'TABLE', @level1name = N'Logs', @level2type = N'INDEX', @level2name = N'CCX_Application_Logs';
 
 
 GO
@@ -30,7 +30,7 @@ EXECUTE sp_addextendedproperty @name = N'Description', @value = 'Severity of the
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'Description', @value = 'Time when the record is logged', @level0type = N'SCHEMA', @level0name = N'Application', @level1type = N'TABLE', @level1name = N'Logs', @level2type = N'COLUMN', @level2name = N'TimeStamp';
+EXECUTE sp_addextendedproperty @name = N'Description', @value = 'Time when the record is logged', @level0type = N'SCHEMA', @level0name = N'Application', @level1type = N'TABLE', @level1name = N'Logs', @level2type = N'COLUMN', @level2name = 'EventTime';
 
 
 GO
