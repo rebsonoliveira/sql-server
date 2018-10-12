148ce3398086db39b6320eafc094481b4a70105b
Umachandar Jayachandran <umajay@microsoft.com>
2018-10-12 12:16:00 -0700

    Added instructions for setting up kubeadm cluster

---
+++ samples/features/sql-big-data-cluster/deployment/kubeadm/setup-k8s-prereqs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -x
+
+# Setup the kubernetes preprequisites
+#
+sudo -i
+echo $(hostname -i) $(hostname) >> /etc/hosts
+sed -i "/ swap / s/^/#/" /etc/fstab
+swapoff -a
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+KUBE_DPKG_VERSION=1.11.3-00
+apt-get update
+apt-get install -y ebtables ethtool
+apt-get install -y docker.io
+apt-get install -y apt-transport-https
+apt-get install -y kubelet=$KUBE_DPKG_VERSION kubeadm=$KUBE_DPKG_VERSION kubectl=$KUBE_DPKG_VERSION
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+sysctl net.bridge.bridge-nf-call-iptables=1
