84b6ef7c72d64c04bbf9af4bb381dc913efe6448
Srđan Božović <srdjan.bozovic@live.com>
2019-01-27 23:51:20 +0100

    add: initial configuration

---
+++ samples/manage/azure-sql-db-managed-instance/automation-functions/src/ManagedInstanceAutomation/Shared/DirectoryRoles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagedInstanceAutomation.Shared
+{
+    public class DirectoryRole
+    {
+        public string ObjectType { get; set; }
+        public string ObjectId { get; set; }
+        public object DeletionTimestamp { get; set; }
+        public string Description { get; set; }
+        public string DisplayName { get; set; }
+        public bool IsSystem { get; set; }
+        public bool RoleDisabled { get; set; }
+        public string RoleTemplateId { get; set; }
+    }
+
+    public class DirectoryRoles
+    {
+        public DirectoryRole[] Value { get; set; }
+    }
+}
