50f3e90c9b1d11c88a9f85ca1dfe417367964043
Mihaela Blendea <mihaelab@microsoft.com>
2019-03-04 09:21:16 -0800

    Update deploy-sql-big-data-aks.py

--- samples/features/sql-big-data-cluster/deployment/aks/deploy-sql-big-data-aks.py
+++ samples/features/sql-big-data-cluster/deployment/aks/deploy-sql-big-data-aks.py
@@ -69,7 +69,7 @@ command="az group create --name "+GROUP_NAME+" --location "+AZURE_REGION
 executeCmd (command)
 
 print("Creating AKS cluster: "+CLUSTER_NAME)
-command = "az aks create --name "+CLUSTER_NAME+" --resource-group "+GROUP_NAME+" --generate-ssh-keys --node-vm-size "+VM_SIZE+" --node-count "+AKS_NODE_COUNT+" --kubernetes-version 1.10.9"
+command = "az aks create --name "+CLUSTER_NAME+" --resource-group "+GROUP_NAME+" --generate-ssh-keys --node-vm-size "+VM_SIZE+" --node-count "+AKS_NODE_COUNT+" --kubernetes-version 1.10.12"
 executeCmd (command)
 
 command = "az aks get-credentials --overwrite-existing --name "+CLUSTER_NAME+" --resource-group "+GROUP_NAME+" --admin"
