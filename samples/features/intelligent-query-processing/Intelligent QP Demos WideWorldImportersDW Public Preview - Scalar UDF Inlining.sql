5b68cea3c4068320afa4dec9cbd7b134f25825bd
Pedro Lopes <pedro.lopes@microsoft.com>
2019-06-13 10:43:19 -0400

    Updates to Scalar UDF inlining Demo

--- samples/features/intelligent-query-processing/Intelligent QP Demos WideWorldImportersDW Public Preview - Scalar UDF Inlining.sql
+++ samples/features/intelligent-query-processing/Intelligent QP Demos WideWorldImportersDW Public Preview - Scalar UDF Inlining.sql
@@ -26,7 +26,7 @@ Adapted from SQL Server Books Online
 https://docs.microsoft.com/sql/relational-databases/user-defined-functions/scalar-udf-inlining?view=sqlallproducts-allversions 
 */
 CREATE OR ALTER FUNCTION 
-	dbo.customer_category(@CustomerKey INT) 
+	dbo.ufn_customer_category(@CustomerKey INT) 
 RETURNS CHAR(10) AS
 BEGIN
   DECLARE @total_amount DECIMAL(18,2);
@@ -48,20 +48,25 @@ BEGIN
 END
 GO
 
+SELECT * FROM sys.sql_modules
+WHERE object_id = OBJECT_ID('ufn_customer_category')
+GO
+
 -- Before (show actual query execution plan for legacy behavior)
 SELECT TOP 100
 		[Customer Key], [Customer],
-       dbo.customer_category([Customer Key]) AS [Discount Price]
+       dbo.ufn_customer_category([Customer Key]) AS [Discount Price]
 FROM [Dimension].[Customer]
 ORDER BY [Customer Key]
 OPTION (RECOMPILE,USE HINT('DISABLE_TSQL_SCALAR_UDF_INLINING'));
+GO
 
 -- After (show actual query execution plan for Scalar UDF Inlining)
 SELECT TOP 100
 		[Customer Key], [Customer],
-       dbo.customer_category([Customer Key]) AS [Discount Price]
+       dbo.ufn_customer_category([Customer Key]) AS [Discount Price]
 FROM [Dimension].[Customer]
 ORDER BY [Customer Key]
 OPTION (RECOMPILE);
-
+GO
 
