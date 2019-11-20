aedcb84f2db091db6ebbb962e26ce9de0e85b009
Guillaume-Fourrat <39527431+Guillaume-Fourrat@users.noreply.github.com>
2019-11-20 14:57:49 +0100

    use >> (append) instead of > for sysctl.conf updates
    I'm afraid current script would clobber existing /etc/sysctl.conf configuration
    AND each of the 3 changes clobbering would clobber the previous one(s), only the last "net.ipv6.conf.lo.disable_ipv6=1" would be persisted in sysctl.

--- samples/features/sql-big-data-cluster/deployment/kubeadm/ubuntu-single-node-vm-ad/setup-bdc-ad.sh
+++ samples/features/sql-big-data-cluster/deployment/kubeadm/ubuntu-single-node-vm-ad/setup-bdc-ad.sh
@@ -226,9 +226,9 @@ sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
 sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 
-echo net.ipv6.conf.all.disable_ipv6=1 > /etc/sysctl.conf
-echo net.ipv6.conf.default.disable_ipv6=1 > /etc/sysctl.conf
-echo net.ipv6.conf.lo.disable_ipv6=1 > /etc/sysctl.conf
+echo net.ipv6.conf.all.disable_ipv6=1 >> /etc/sysctl.conf
+echo net.ipv6.conf.default.disable_ipv6=1 >> /etc/sysctl.conf
+echo net.ipv6.conf.lo.disable_ipv6=1 >> /etc/sysctl.conf
 
 
 sysctl net.bridge.bridge-nf-call-iptables=1
