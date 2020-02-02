4d89d55bad789e91834445cdee0e35bef2a99a89
ananto-msft <57770929+ananto-msft@users.noreply.github.com>
2020-02-02 09:09:01 -0800

    Updating the PV count to 80.

--- samples/features/azure-arc/deployment/kubeadm/ubuntu-single-node-vm/setup-controller.sh
+++ samples/features/azure-arc/deployment/kubeadm/ubuntu-single-node-vm/setup-controller.sh
@@ -65,7 +65,7 @@ RETRY_INTERVAL=5
 #
 export ACCEPT_EULA=yes
 export CLUSTER_NAME=azure-arc-system
-export PV_COUNT="40"
+export PV_COUNT="80"
 
 # Make a directory for installing the scripts and logs.
 #
